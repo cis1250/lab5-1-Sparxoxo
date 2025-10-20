ee6f12a24c4b0158daba8e5f9611d167e5673086
Amanda Parco <amandaparco@gmail.com>
2025-10-20 13:13:57 -0400

    Add step-by-step comments for word frequency task
    Added comments outlining the steps for the word frequency exercise.

--- word_frequency.py
+++ word_frequency.py
@@ -2,6 +2,10 @@
 
 # Word frequency exercise
 # TODO: (Read detailed instructions in the Readme file)import string
+# 1 Prompt the user: Ask the user to enter a sentence.
+# 2. Split the sentence
+# 3. Create lists to store words and their corresponding frequencies.
+# 4. Iterate through words and update frequencies.
 
 import re
 
